9a38ffd02d324eea7473105cc3104b97c47bfced
Lonny Gomes <code@lonnygomes.com>
2021-05-18 22:41:47 -0400

    chore: prettierfy gen-dummy-data.js

--- src/gen-dummy-data.js
+++ src/gen-dummy-data.js
@@ -6,12 +6,16 @@ const COUNT = 10_000_000;
 const stream = fs.createWriteStream(`input.csv`);
 
 const rand = (max) => {
-  return Math.round(Math.random() * max);
-}
+    return Math.round(Math.random() * max);
+};
 stream.write(cols);
 
 for (let idx = 0; idx < COUNT; idx += 1) {
-  stream.write(`${rand(100)},${rand(100)},${rand(100)},${rand(100)},${rand(255)},${rand(255)},${rand(255)}\n`);
+    stream.write(
+        `${rand(100)},${rand(100)},${rand(100)},${rand(100)},${rand(
+            255
+        )},${rand(255)},${rand(255)}\n`
+    );
 }
 
 stream.end();
