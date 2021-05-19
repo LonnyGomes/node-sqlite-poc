bce5a4ca63b55ac359cb413ad4349700a1aa7fc3
Lonny Gomes <code@lonnygomes.com>
2021-05-18 22:30:04 -0400

    chore: generate dummy csv data utility

---
+++ src/gen-dummy-data.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const cols = 'c,m,y,k,r,g,b\n';
+const COUNT = 10_000_000;
+
+const stream = fs.createWriteStream(`input.csv`);
+
+const rand = (max) => {
+  return Math.round(Math.random() * max);
+}
+stream.write(cols);
+
+for (let idx = 0; idx < COUNT; idx += 1) {
+  stream.write(`${rand(100)},${rand(100)},${rand(100)},${rand(100)},${rand(255)},${rand(255)},${rand(255)}\n`);
+}
+
+stream.end();
